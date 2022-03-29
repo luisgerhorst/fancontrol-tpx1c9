82e60438d06b90db83b5862961475196c70591ab
Luis Gerhorst <gerhorst@cs.fau.de>
2022-03-29 17:29:54 +0200

    steep

--- fancontrol-tpx1cp.py
+++ fancontrol-tpx1cp.py
@@ -12,13 +12,15 @@ def main() -> int:
     pwm_path=Path("/sys/class/hwmon/hwmon5/pwm1")
     pwm_enable_path=Path("/sys/class/hwmon/hwmon5/pwm1_enable")
 
-    sec_temp_path=Path("/sys/class/hwmon/hwmon5/temp1_input")
+    # sec_temp_path=Path("/sys/class/hwmon/hwmon5/temp1_input")
 
     interval=2
     # (enable_temp, pwm): increase to pwm if > enable_temp, decrease to pwm if < enable_temp
     tp_map = [(0, 0),
               (55, 0),
               (65, 40),   # 4500RPM
+              (75, 80),   # 4800RPM
+              (85, 120),
               (95, 255)]
     max_pwm_delta = 5 # only change by x pwm per interval
 
@@ -35,13 +37,15 @@ def main() -> int:
                 if temp >= t:
                     level_temp = t
                     pwm = p
+
+        # Only reduce level if we are below the lower level's enable_temp.
         if temp < level_temp:
             for (t,p) in reversed(tp_map):
                 if temp < t:
                     level_temp = t
                     pwm = p
 
-        # Limit rate of change.
+        # Limit rate of pwm increase to prevent over-compensation.
         max_pwm = last_pwm + max_pwm_delta
         if pwm > max_pwm:
             pwm = max_pwm
